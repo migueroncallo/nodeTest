aeac2108200ed9e6075f0260ade462f69d3ff12f
Apple <apple@Apples-MacBook-Pro.local>
2017-11-01 10:42:51 -0500

    Add server response

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var express = require('express')
+var MongoClient = require('mongodb')
 
 const port = process.env.PORT || 3000
 
 var app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send({
+    message: 'Hello World'
+  })
+})
+
+
+app.listen(port, () =>{
+  console.log('Server up on port', port)
 })
